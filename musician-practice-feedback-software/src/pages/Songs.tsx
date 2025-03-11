4328454fdacb10332d790ae84845fb45443e0814
cameronevans26 <158220287+cameronevans26@users.noreply.github.com>
2025-03-10 22:05:59 -0500

    Fix songs.tsx

--- musician-practice-feedback-software/src/pages/Songs.tsx
+++ musician-practice-feedback-software/src/pages/Songs.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Songs.css';
 
 const Songs = () => {
   return (
-    <div>
-      <h1>Songs</h1>
-      <p>This is where the user will select songs.</p>
+    <div className="songs-container">
+      <h1 className="songs-title">Songs</h1>
+      <p className="songs-description">This is where the user will select songs.</p>
     </div>
   );
 };

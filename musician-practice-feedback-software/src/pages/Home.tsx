ed4ba51657d1539ad14226d47ccb0052619758db
cameronevans26 <158220287+cameronevans26@users.noreply.github.com>
2025-03-10 21:47:07 -0500

    Cool Home Screen (#2)
    * Add .css and colors
    
    * New Home

--- musician-practice-feedback-software/src/pages/Home.tsx
+++ musician-practice-feedback-software/src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Musician Practice Feedback Software!</h1>
-      <p>The best practice software on the planet.</p>
+    <div className="home-container">
+    <div className="image-overlay">
+      <img src="/CoolMusic.png" alt="Background" />
+    </div>
+      <h1 className="home-title">Welcome to Musician Practice Feedback Software!</h1>
+      <p className="home-description">The best practice software on the planet.</p>
     </div>
   );
 };
